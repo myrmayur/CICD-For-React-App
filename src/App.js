2622cb08d321caf17c56e785fe4bc6dfdf9fb5d2
myrmayur <mayurwaghmare358@gmail.com>
2024-08-02 12:43:20 +0000

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> changes make for testing the webhook trigger Test 1
+          Edit <code>src/App.js</code> changes make for testing the webhook trigger Test 2
         </p>
         <a
           className="App-link"
